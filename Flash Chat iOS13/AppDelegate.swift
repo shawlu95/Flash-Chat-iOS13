64239b19babe9369388b03b6cf8b139e9ebeddb7
Shaw Lu <shawlu95@gmail.com>
2023-06-13 02:21:32 -0700

    save message to firestore

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import FirebaseCore
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        let db = Firestore.firestore()
         return true
     }
 
