521a581cc49e94c33c18ddbff4ca2190e8e43c37
Shaw Lu <shawlu95@gmail.com>
2023-06-13 11:43:38 -0700

    beautify navbar

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -24,7 +24,6 @@ class ChatViewController: UIViewController {
         
         tableView.dataSource = self
         
-        title = K.appName
         navigationItem.hidesBackButton = true
         
         tableView.register(
