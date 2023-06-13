e8c236adb686bddcdd89d486a577739d8edaf4c9
Shaw Lu <shawlu95@gmail.com>
2023-06-13 01:24:12 -0700

    refactor strings into constant struct

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by main on 6/13/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let appName = "⚡️FlashChat"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+}
